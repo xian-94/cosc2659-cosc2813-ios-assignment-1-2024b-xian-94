93534c6d1bd994409d7259936bd9743326d39c01
Xian <hongvanct2003@gmail.com>
2024-07-20 16:34:30 +0700

    init project

---
+++ CocktailRecipe/CocktailRecipe/CocktailRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailRecipeApp.swift
+//  CocktailRecipe
+//
+//  Created by Do Le Long An on 20/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CocktailRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CocktailRecipe/CocktailRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CocktailRecipe
+//
+//  Created by Do Le Long An on 20/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
